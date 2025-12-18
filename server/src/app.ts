e25a38efac6f4f5ba2d8f05b2e71d4d0e0ccbcf1
Rose Goldfarb <rose.goldfarb26@gmail.com>
2025-12-18 16:16:25 -0500

    adds post endpoint for creating playlist based on prompt

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import playlistRoutes from "./routes/playlistGenerationRoutes"
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/playlist", playlistRoutes)
 
 app.get("/health", (req, res) => {
   res.json({ status: "Server is running" });
